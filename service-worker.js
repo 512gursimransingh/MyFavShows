21b9a7ff88b1122df29d596581fbd2c91a94bdf2
512gursimransingh <gsimran007@gmail.com>
2025-09-17 18:42:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'hindi-movie-channels-yt-ui-v1';
-const urlsToCache = ['./','./index.html','./style.css','./script.js','./channels.json','./manifest.json'];
+// const CACHE_NAME = 'hindi-movie-channels-yt-ui-v1';
+// const urlsToCache = ['./','./index.html','./style.css','./script.js','./channels.json','./manifest.json'];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
-});
+// self.addEventListener('install', e=>{
+//   e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+// });
 
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));
-});
+// self.addEventListener('activate', e=>{
+//   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));
+// });
 
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+// self.addEventListener('fetch', e=>{
+//   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+// });
